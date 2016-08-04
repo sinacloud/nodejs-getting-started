19dcf6868947dc957fc949e2c8c18d554589bfe4
chenfeng2 <chenfeng2@staff.sina.com.cn>
2016-08-04 11:08:15 +0800

    环境变量没有添加REDIS_URL的时候不注册adapter

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var redis = require('socket.io-redis');
-io.adapter(redis(process.env.REDIS_URL, {}));
+if (process.env.REDIS_URL) {
+    console.log('install socket.io-redis adapter');
+    io.adapter(redis(process.env.REDIS_URL, {}));
+}
 var port = process.env.PORT || 5050;
 
 server.listen(port, function () {
